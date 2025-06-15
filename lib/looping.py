f8ab4c5e3cc04a08552c5422b1093f56ef3dd7b0
Ellis Lunayo <ellislunayo@gmail.com>
2025-06-15 22:43:53 +0300

    New loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i > 0:
+        print(i)
+        i -=1
+        print ("Happy New Year!")
+        
+
 
 def square_integers(int_list):
-    # code goes here!
+    return list(map(lambda num: num** 2 , int_list))
     pass
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+    
+   
+   
+
+# players_heights = [0.008, 0.008, 0.008, 0.009, 0.008, 0.01, 0.009, 0.009, 0.01, 0.008, 0.009, 0.009, 0.008, 0.008, 0.008, 0.009, 0.008, 0.009, 0.01, 0.01]
+
+# inch_heights = [height * 7920 for height in players_heights]
+# print(inch_heights)
+
+
+# for i in range(10):
+#     print(f"Looping {i} times")
